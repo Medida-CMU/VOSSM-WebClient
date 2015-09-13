e1649777b89dea121a556e36797a91c3923efab1
yesyayen <yesyayen@gmail.com>
2015-09-13 11:59:02 -0400

    fixed unit test issue of mongoengine changed TESTRUNNER

--- vossm_project/settings.py
+++ vossm_project/settings.py
@@ -104,17 +104,14 @@ TEMPLATE_DIRS = (
     )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'vossm_websiteApp'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+TEST_RUNNER = 'testrunner.run_tests'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
--- vossm_project/vossm_websiteApp/tests.py
+++ vossm_project/vossm_websiteApp/tests.py
@@ -6,9 +6,17 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from django.conf import settings
 
 class SimpleTest(TestCase):
+
+	    # Stop ORM-related stuff from happening as we don't use the ORM
+    def _fixture_setup(self):
+        pass
+    def _fixture_teardown(self):
+        pass
+
+
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
