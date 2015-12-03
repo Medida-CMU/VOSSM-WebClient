359675daa15cf70f706eb1ee58a242c99b884379
preethibaskar <preethibaskar90@gmail.com>
2015-12-03 17:39:43 -0500

    Fetching data from db

--- vossm_websiteApp/configuredata.py
+++ vossm_websiteApp/configuredata.py
@@ -3,20 +3,37 @@ from pymongo import MongoClient
 #from pymongo import Connection
 from django.shortcuts import render
 
-database_name="vossmDB"
-collection_name="Users"
+database_name="VOSSM"
+collection_name="raw_data"
 
 def configure(request):
-    #connection = Connection()
+	data = {}
+	count =1
+	version = []
+	connection = MongoClient()
+	db = connection.VOSSM
+	collection = db[collection_name]
+	dataCursor = collection.find()
+	for record in dataCursor:
+		data[count] = record
+		count = count+1
 
-    #db = connection[database_name]
-    connection = MongoClient()
-    db = connection.vossmDB
-    collection = db[collection_name]
+		print version
 
-    data={}
-    data['count']=collection.find().count();
+	print data
+	return render(request, 'configuredata.html', {"data" : data})
 
-    print data
+# def configure(request):
+#     #connection = Connection()
+
+#     #db = connection[database_name]
+#     connection = MongoClient()
+#     db = connection.vossmDB
+#     collection = db[collection_name]
+
+#     data={}
+#     data['count']=collection.find().count();
+
+#     print data
     
-    return render(request, 'configuredata.html', {})
+#     return render(request, 'configuredata.html', {})
