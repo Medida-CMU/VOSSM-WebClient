c25fa15255bf382b573b99d00f1ef3fb4386d44b
Sivaraman <sivaramansv>
2015-12-05 00:06:05 -0500

    Modified filter functionality

--- vossm_websiteApp/sharedata.py
+++ vossm_websiteApp/sharedata.py
@@ -20,10 +20,9 @@ database_name="vossmDB"
 collection_name="Data"
 
 def share(request):
-    #connection = Connection()
+
     connection = MongoClient()
     db = connection.vossmDB
-    #db = connection[database_name]
     collection = db[collection_name]
 
     data= {}
@@ -33,38 +32,6 @@ def share(request):
     for record in cursor:
         data[count]=record
         count=count+1
-    print 'Get method'
-    print data
     
     
     return render(request, 'share.html', {'data': data})
-
-@csrf_exempt
-def filter(request):
-    #connection = Connection()
-    #db = connection[database_name]
-    connection = MongoClient()
-    db = connection.vossmDB
-    collection = db[collection_name]
-    
-    data={}
-    response_array=[]
-    if request.method == 'POST' and request.is_ajax():
-        recieved_json_data=json.loads(request.raw_post_data)
-        packageName=recieved_json_data['packageName']
-        
-    response_data={}
-    cursor=collection.find({'PackageName':packageName});
-    count=0
-    for record in cursor:
-        response_array.append(record)
-        data[count]=record
-        count=count+1
-    #data['records']=response_array
-    #result=data  
-    print 'Filter:'
-    print data
-    #return HttpResponse(json.dumps(result),content_type="application/json")
-    return render(request, 'share.html', {'data': data})        
-    #return HttpResponse(json.dumps(response_data),content_type="application/json")
-    #return render_to_response('share.html', c, RequestContext(request))
